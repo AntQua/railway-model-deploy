48feb5d6077d208a718277edce2f231d69afa156
Antonio Q <tozequa@gmail.com>
2026-03-24 21:42:23 +0000

    fix port for railway

--- app.py
+++ app.py
@@ -216,4 +216,4 @@ def update():
 
 if __name__ == "__main__":
     initialize_db()
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
